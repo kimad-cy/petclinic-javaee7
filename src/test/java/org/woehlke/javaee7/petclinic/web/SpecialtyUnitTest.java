fd3fc0cb1624b137b3f56949598c101beea07c3a
Khadija Imad <khadija.imad@um6p.ma>
2025-11-09 17:23:13 +0100

    tests issue again

---
+++ src/test/java/org/woehlke/javaee7/petclinic/web/SpecialtyUnitTest.java
@@ -0,0 +1,14 @@
+package org.woehlke.javaee7.petclinic.web;
+
+import org.junit.Test;
+
+
+public class SpecialtyUnitTest {
+    //dummy test
+    @Test
+    public void testSpecialtyCreation() {
+       
+    }
+    
+    
+}
